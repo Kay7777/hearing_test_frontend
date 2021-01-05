1ca10e6457eb80dce1e60959beb0f9ba140b5026
Kay <kay7song@gmail.com>
2021-01-04 18:09:37 -0700

    add build

---
+++ build/precache-manifest.404b1ba37e2d42d384fa158be2161307.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56da50af4d1b829c6129567279513b88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfe434d649acf0bd341e",
+    "url": "/static/js/2.6efc6aec.chunk.js"
+  },
+  {
+    "revision": "5a1f67c30be520163768425364e600bd",
+    "url": "/static/js/2.6efc6aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f2040c226415b2b6324",
+    "url": "/static/js/main.d58ae77a.chunk.js"
+  },
+  {
+    "revision": "db7c1494e2368a624902fd6660d69417",
+    "url": "/static/js/pdf.worker.entry.fa64aa01.worker.js"
+  },
+  {
+    "revision": "ac3df35bcd7a2aa579e062210ce2d521",
+    "url": "/static/js/pdf.worker.entry.fa64aa01.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "c76d232ed1d5461d5631",
+    "url": "/static/js/runtime-main.2006e9ed.js"
+  }
+]);
