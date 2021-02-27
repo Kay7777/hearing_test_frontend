b592bcf2b7140fe2fc2967147760936d7abd88dd
Kay7777 <kay7song@gmail.com>
2021-02-27 13:01:32 -0700

    add 4 post questions

--- src/pages/crm1/main.jsx
+++ src/pages/crm1/main.jsx
@@ -11,7 +11,10 @@ import End from "../../components/crm1/post-test/end";
 import ICF from "../../components/crm1/pre-test/ICF";
 import DemoInfo from "../../components/crm1/pre-test/demo-info";
 import MidPage from "../../components/crm1/test/mid-page";
-import PostQuestion from "../../components/crm1/post-test/questions";
+import PostQuestion1 from "../../components/crm1/post-test/questions1";
+import PostQuestion2 from "../../components/crm1/post-test/questions2";
+import PostQuestion3 from "../../components/crm1/post-test/questions3";
+import PostQuestion4 from "../../components/crm1/post-test/questions4";
 import axios from "axios";
 
 class Main extends React.Component {
@@ -43,7 +46,11 @@ class Main extends React.Component {
       correct2: [],
       correct3: [],
       correct4: [],
-      questions: {},
+      preQuestions: {},
+      postQuestions1: {},
+      postQuestions2: {},
+      postQuestions3: {},
+      postQuestions4: {},
       didWearAids: null,
       willWearAids: null,
       order: [],
@@ -86,42 +93,12 @@ class Main extends React.Component {
   }
 
   handlePreQuestions = (questions, aids) => {
-    this.setState({ questions, didWearAids: aids, process: "adjust" });
+    this.setState({ preQuestions: questions, didWearAids: aids, process: "adjust" });
   }
 
   handleAdjustClick = (output, aids, volume) => {
     console.log("The final volume", volume);
     this.setState({ output, volume, process: "test", willWearAids: aids });
-  }
-
-  handlePostQuestions = async (postQuestion) => {
-    // save data into database
-    const { 
-      output, email, ID, reversals,
-      questions, didWearAids, willWearAids,
-      order, age, gender, province, 
-      timer1, timer2, timer3, timer4, 
-      dbs1, dbs2, dbs3, dbs4, 
-      questions1, questions2, questions3, questions4,
-      answers1, answers2, answers3, answers4,
-      correct1, correct2, correct3, correct4
-    } = this.state;
-    const trials1 = timer1.length;
-    const trials2 = timer2.length;
-    const trials3 = timer3.length;
-    const trials4 = timer4.length;
-    await axios.post("/api/sentence/user/data", {
-      output, email, ID, order, 
-      timer1, timer2, timer3, timer4, 
-      dbs1, dbs2, dbs3, dbs4, 
-      preQuestion: questions, postQuestion, didWearAids, 
-      willWearAids, SNR: this.state.SNR, age, gender, province, 
-      questions1, questions2, questions3, questions4,
-      answers1, answers2, answers3, answers4,
-      correct1, correct2, correct3, correct4, 
-      reversals, trials1, trials2, trials3, trials4
-    });
-    this.setState({ process: "end" });
   }
 
   handleTest = () => {
@@ -131,22 +108,22 @@ class Main extends React.Component {
   handleCRM1Click = (SNR, timer, dbs, questions, answers, correct) => {
     const newSNR = this.state.SNR;
     newSNR.push(SNR);
-    this.setState({ SNR: newSNR, timer1: timer, dbs1: dbs, questions1: questions, answers1: answers, correct1: correct, process: "midpage1" })
+    this.setState({ SNR: newSNR, timer1: timer, dbs1: dbs, questions1: questions, answers1: answers, correct1: correct, process: "postquestion1" })
   }
   handleCRM2Click = (SNR, timer, dbs, questions, answers, correct) => {
     const newSNR = this.state.SNR;
     newSNR.push(SNR);
-    this.setState({ SNR: newSNR, timer2: timer, dbs2: dbs, questions2: questions, answers2: answers, correct2: correct, process: "midpage2" })
+    this.setState({ SNR: newSNR, timer2: timer, dbs2: dbs, questions2: questions, answers2: answers, correct2: correct, process: "postquestion2" })
   }
   handleCRM3Click = (SNR, timer, dbs, questions, answers, correct) => {
     const newSNR = this.state.SNR;
     newSNR.push(SNR);
-    this.setState({ SNR: newSNR, timer3: timer, dbs3: dbs, questions3: questions, answers3: answers, correct3: correct, process: "midpage3" })
+    this.setState({ SNR: newSNR, timer3: timer, dbs3: dbs, questions3: questions, answers3: answers, correct3: correct, process: "postquestion3" })
   }
   handleCRM4Click = async (SNR, timer, dbs, questions, answers, correct) => {
     const newSNR = this.state.SNR;
     newSNR.push(SNR);
-    this.setState({ SNR: newSNR, timer4: timer, dbs4: dbs, questions4: questions, answers4: answers, correct4: correct, process: "postquestion" })
+    this.setState({ SNR: newSNR, timer4: timer, dbs4: dbs, questions4: questions, answers4: answers, correct4: correct, process: "postquestion4" })
   }
 
   handleMidPage1Click = () => {
@@ -159,6 +136,54 @@ class Main extends React.Component {
 
   handleMidPage3Click = () => {
     this.setState({ process: "CRM4" })
+  }
+
+
+  handlePostQuestions1 = (postQuestion) => {
+    this.setState({postQuestion1: postQuestion, process: "midpage1"})
+  }
+
+  handlePostQuestions2 = (postQuestion) => {
+    this.setState({postQuestion2: postQuestion, process: "midpage2"})
+  }
+
+  handlePostQuestions3 = (postQuestion) => {
+    this.setState({postQuestion3: postQuestion, process: "midpage3"})
+  }
+
+  handlePostQuestions4 = async (postQuestion) => {
+    await this.setState({ postQuestion4: postQuestion });
+    await this.handleSubmit(() => this.setState({process: "end"}));
+  }
+
+  handleSubmit = async (callback) => {
+    const { 
+      output, email, ID, reversals, SNR,
+      didWearAids, willWearAids, preQuestions,
+      order, age, gender, province, 
+      timer1, timer2, timer3, timer4, 
+      dbs1, dbs2, dbs3, dbs4, 
+      questions1, questions2, questions3, questions4,
+      postQuestions1, postQuestions2, postQuestions3, postQuestions4,
+      answers1, answers2, answers3, answers4,
+      correct1, correct2, correct3, correct4
+    } = this.state;
+    const trials1 = timer1.length;
+    const trials2 = timer2.length;
+    const trials3 = timer3.length;
+    const trials4 = timer4.length;
+    await axios.post("/api/sentence/user/data", {
+      output, email, ID, order, preQuestions, didWearAids, 
+      willWearAids, SNR, age, gender, province, 
+      timer1, timer2, timer3, timer4, 
+      dbs1, dbs2, dbs3, dbs4, 
+      questions1, questions2, questions3, questions4,
+      postQuestions1, postQuestions2, postQuestions3, postQuestions4,
+      answers1, answers2, answers3, answers4,
+      correct1, correct2, correct3, correct4, 
+      reversals, trials1, trials2, trials3, trials4
+    });
+    callback();
   }
 
   returnTrainingMode = (num, func) => {
@@ -191,20 +216,26 @@ class Main extends React.Component {
         return <Test volume={volume} handleClick={this.handleTest} />
       case "CRM1":
         return this.returnTrainingMode(0, this.handleCRM1Click);
+      case "postquestion1":
+        return <PostQuestion1 handleClick={this.handlePostQuestions1} />
       case "midpage1":
         return <MidPage handleClick={this.handleMidPage1Click} section={"two"} />;
       case "CRM2":
         return this.returnTrainingMode(1, this.handleCRM2Click);
+      case "postquestion2":
+        return <PostQuestion2 handleClick={this.handlePostQuestions2} />
       case "midpage2":
         return <MidPage handleClick={this.handleMidPage2Click} section={"three"} />;
       case "CRM3":
         return this.returnTrainingMode(2, this.handleCRM3Click);
+      case "postquestion3":
+        return <PostQuestion3 handleClick={this.handlePostQuestions3} />
       case "midpage3":
         return <MidPage handleClick={this.handleMidPage3Click} section={"four"} />;
       case "CRM4":
         return this.returnTrainingMode(3, this.handleCRM4Click);
-      case "postquestion":
-        return <PostQuestion handleClick={this.handlePostQuestions} />
+      case "postquestion4":
+        return <PostQuestion4 handleClick={this.handlePostQuestions4} />
       case "end":
         return <End />
       default:
